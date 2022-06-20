70df60c3184775883246faf2ce0bd97e24ad2eb5
aalsu <aalsulami35@gmail.com>
2022-06-20 23:58:33 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import Card from './components/CardComponent/Card';
+import Cat1 from './components/assets/img/cat1.jpg';
+import Cat2 from './components/assets/img/cat2.jpg'
+import Cat3 from './components/assets/img/cat3.jpg'
+import Cat4 from './components/assets/img/cat4.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Cat Stor</h1>
+    <div className="row justify-content-center">
+    <div className="col">
+    <Card name="Tome" age="1" imgSrc={Cat1} imgAlt="Cat 1 image"/>
+    </div>
+    <div className="col">
+    <Card name="Hary" age="2" imgSrc={Cat2} imgAlt="Cat 2 image"/>
+    </div>
+    <div className="w-100"></div>
+    <div className="col">
+    <Card name="Bsbs" age="3" imgSrc={Cat3} imgAlt="Cat 3 image"/>
+    </div>
+    <div className="col">
+    <Card name="May" age="4" imgSrc={Cat4} imgAlt="Cat 4 image" />
+    </div>
+  </div>
+    </div>
+  
+  );
+}
+/* 
+    
+      
+      
+     
+*/
+
+export default App;
---
+++ src/components/CardComponent/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styls from './Card.module.css'
+
+
+function Card(props) {
+  return (
+    <div className="card" style={{}}>
+      <img id="catImg" src={props.imgSrc} alt={props.imaAlt} className={`card-img-top ${styls.aa}`}/>
+      <div className="card-body">
+        <h2 className="card-title">name: {props.name}</h2>
+        <p className="card-text">age: {props.age}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
